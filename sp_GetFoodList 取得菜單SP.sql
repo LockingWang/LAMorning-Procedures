a7103121bc5d08b1f4b29eaec3760063013f9421
nextxurf <jingker@cloudxurf.com>
2025-07-01 11:53:24 +0800

    更新

--- sp_GetFoodList 取得菜單SP.sql
+++ sp_GetFoodList 取得菜單SP.sql
@@ -1,181 +1,182 @@
-CREATE OR ALTER PROCEDURE sp_GetFoodList     
-    @enterpriseId NVARCHAR(50),    -- 企業ID     
-    @shopId NVARCHAR(50),          -- 店鋪ID      
-    @categoryId NVARCHAR(50),      -- 分類ID（目前未使用）     
-    @orderType NVARCHAR(50),       -- 訂單類型（目前未使用）     
-    @langId NVARCHAR(50),          -- 語系ID     
-    @foodId NVARCHAR(50) = NULL    -- 食品ID 
-AS    
-
--- 測試用    
--- DECLARE @enterpriseId NVARCHAR(50) = '90367984'     -- 企業ID     
--- DECLARE @shopId NVARCHAR(50) = 'A01'          -- 店鋪ID      
--- DECLARE @categoryId NVARCHAR(50) = ''           -- 分類ID（目前未使用）     
--- DECLARE @orderType NVARCHAR(50) = 'scaneDesk'       -- 訂單類型（目前未使用）     
--- DECLARE @langId NVARCHAR(50) = 'TW'          -- 語系ID     
--- DECLARE @foodId NVARCHAR(50) = NULL    -- 食品ID 
-
-
-BEGIN     
-    SET NOCOUNT ON;     
-     
-    BEGIN TRY     
-     
-SELECT     
-    FM.Kind AS FoodCategoryId,  -- 食品分類ID     
-    ISNULL(JSON_VALUE(LANGKIND.Content, '$.' + @langId + '.Name'), FK.Name) AS FoodCategoryName,  -- 食品分類名稱（多語系）    
-    FK.Sn AS FoodCategorySort,  -- 食品分類排序     
-    FM.ID AS FoodId,            -- 食品ID     
-    ISNULL(JSON_VALUE(LANGFOOD.Content, '$.' + @langId + '.Name'), FM.Name) AS FoodName,          -- 食品名稱（多語系）     
-    (      
-        -- 取食品圖片路徑（僅取一筆）     
-        SELECT TOP 1 Dir      
-        FROM S_UploadFile UF     
-        WHERE UF.enterpriseid = @enterpriseId AND UF.itemid = FM.ID      
-    ) AS ImagePath,     
-    F.Introduce AS Description, -- 食品描述     
-    CASE WHEN FM.YSFlag = 1 THEN 1 ELSE 0 END AS IsCombo,  -- 是否為套餐（YSFlag=1為套餐，0為單品）     
-    FM.price AS Price,           -- 價格
-    -- 套餐升級清單（找出包含當前商品的套餐）
-    CASE 
-        WHEN FM.YSFlag = 0 THEN  -- 只有單品才需要找套餐
-            CONCAT('[', 
-                STUFF((
-                    SELECT DISTINCT
-                        ',{"FoodId":"' + CAST(PFM.ID AS NVARCHAR(50)) + '","FoodName":"' + ISNULL(JSON_VALUE(LANGPFM.Content, '$.' + @langId + '.Name'), PFM.Name) + '","Price":' + CAST(PFM.price AS NVARCHAR(20)) + '}'
-                    FROM P_FoodEnt_Mould PFEM
-                    JOIN P_FoodMould PFM ON PFM.EnterpriseID = @enterpriseId 
-                        AND PFM.MouldCode = PFEM.MouldCode 
-                        AND PFM.YSFlag = 1  -- 只找套餐
+CREATE OR ALTER PROCEDURE sp_GetFoodList      
+    @enterpriseId NVARCHAR(50),    -- 企業ID      
+    @shopId NVARCHAR(50),          -- 店鋪ID       
+    @categoryId NVARCHAR(50),      -- 分類ID（目前未使用）      
+    @orderType NVARCHAR(50),       -- 訂單類型（目前未使用）      
+    @langId NVARCHAR(50),          -- 語系ID      
+    @foodId NVARCHAR(50) = NULL    -- 食品ID  
+AS     
+ 
+-- 測試用     
+-- DECLARE @enterpriseId NVARCHAR(50) = '90367984'     -- 企業ID      
+-- DECLARE @shopId NVARCHAR(50) = 'A01'          -- 店鋪ID       
+-- DECLARE @categoryId NVARCHAR(50) = ''           -- 分類ID（目前未使用）      
+-- DECLARE @orderType NVARCHAR(50) = 'scaneDesk'       -- 訂單類型（目前未使用）      
+-- DECLARE @langId NVARCHAR(50) = 'TW'          -- 語系ID      
+-- DECLARE @foodId NVARCHAR(50) = NULL    -- 食品ID  
+ 
+ 
+BEGIN      
+    SET NOCOUNT ON;      
+      
+    BEGIN TRY      
+      
+SELECT      
+    FM.Kind AS FoodCategoryId,  -- 食品分類ID      
+    ISNULL(JSON_VALUE(LANGKIND.Content, '$.' + @langId + '.Name'), FK.Name) AS FoodCategoryName,  -- 食品分類名稱（多語系）     
+    FK.Sn AS FoodCategorySort,  -- 食品分類排序      
+    FM.ID AS FoodId,            -- 食品ID      
+    ISNULL(JSON_VALUE(LANGFOOD.Content, '$.' + @langId + '.Name'), FM.Name) AS FoodName,          -- 食品名稱（多語系）      
+    (       
+        -- 取食品圖片路徑（僅取一筆）      
+        SELECT TOP 1 Dir       
+        FROM S_UploadFile UF      
+        WHERE UF.enterpriseid = @enterpriseId AND UF.itemid = FM.ID       
+    ) AS ImagePath,      
+    F.Introduce AS Description, -- 食品描述      
+    CASE WHEN FM.YSFlag = 1 THEN 1 ELSE 0 END AS IsCombo,  -- 是否為套餐（YSFlag=1為套餐，0為單品）      
+    FM.price AS Price,           -- 價格 
+    -- 套餐升級清單（找出包含當前商品的套餐） 
+    CASE  
+        WHEN FM.YSFlag = 0 THEN  -- 只有單品才需要找套餐 
+            CONCAT('[',  
+                STUFF(( 
+                    SELECT DISTINCT 
+                        ',{"FoodId":"' + CAST(PFM.ID AS NVARCHAR(50)) + '","FoodName":"' + ISNULL(JSON_VALUE(LANGPFM.Content, '$.' + @langId + '.Name'), PFM.Name) + '","Price":' + CAST(PFM.price AS NVARCHAR(20)) + '}' 
+                    FROM P_FoodEnt_Mould PFEM 
+                    JOIN P_FoodMould PFM ON PFM.EnterpriseID = @enterpriseId  
+                        AND PFM.MouldCode = PFEM.MouldCode  
+                        AND PFM.YSFlag = 1  -- 只找套餐 
                         AND PFM.ID = PFEM.MainFood
-                    LEFT JOIN P_Data_Language_D LANGPFM ON LANGPFM.EnterpriseID = @enterpriseId 
-                        AND LANGPFM.SourceID = PFM.ID 
-                        AND LANGPFM.TableName = 'Food'
-                    WHERE PFEM.EnterpriseID = @enterpriseId 
-                        AND PFEM.MouldCode = FMS.MouldCode 
-                        AND PFEM.EntFood = FM.ID  -- 找出包含當前商品的套餐
-                    FOR XML PATH('')
-                ), 1, 1, ''),
-            ']')
-        ELSE '[]'
-    END AS UpgradeList,     
-    FM.Sn AS Sort,               -- 排序     
-     
-    -- 促銷活動清單（若有多個促銷，組成JSON陣列字串）     
-    CASE       
-        WHEN EXISTS (      
-            SELECT 1      
-            FROM p_agiokind gkind_inner      
-            JOIN P_AgioItems aitem       
-                ON gkind_inner.EnterpriseID = @enterpriseId      
-                AND gkind_inner.id = aitem.AgioKind      
-            JOIN P_AgioItems_Shop ashop       
-                ON aitem.EnterpriseID = @enterpriseId      
-                AND aitem.gid = ashop.MGID      
-            JOIN P_Agio_FoodKind_Mould amould       
-                ON amould.EnterpriseID = @enterpriseId      
-                AND amould.MouldCode = aitem.gid      
-            JOIN P_Agio_FoodMould afmould       
-                ON afmould.EnterpriseID = @enterpriseId      
-                AND afmould.MouldCode = amould.MouldCode      
-            WHERE       
-                gkind_inner.EnterpriseID = @enterpriseId      
-                AND ashop.shopid = @shopId      
-                AND aitem.DiscountKind IN ('ASinglePercent', 'AsingleCost')      
-                AND afmould.id = FM.ID     
-        )      
-        THEN CONCAT('[',       
-            STUFF((      
-                SELECT DISTINCT      
-                    ',"' + aitem.Name + '"'      
-                FROM       
-                    p_agiokind gkind_inner      
-                    JOIN P_AgioItems aitem       
-                        ON gkind_inner.EnterpriseID = aitem.EnterpriseID      
-                        AND gkind_inner.id = aitem.AgioKind      
-                    JOIN P_AgioItems_Shop ashop       
-                        ON aitem.EnterpriseID = ashop.EnterpriseID      
-                        AND aitem.gid = ashop.MGID      
-                    JOIN P_Agio_FoodKind_Mould amould       
-                        ON amould.EnterpriseID = ashop.EnterpriseID      
-                        AND amould.MouldCode = aitem.gid      
-                    JOIN P_Agio_FoodMould afmould       
-                        ON afmould.EnterpriseID = ashop.EnterpriseID      
-                        AND afmould.MouldCode = amould.MouldCode      
-                WHERE       
-                    gkind_inner.EnterpriseID = @enterpriseId      
-                    AND ashop.shopid = @shopId      
-                    AND aitem.DiscountKind IN ('ASinglePercent', 'AsingleCost')      
-                    AND afmould.id = FM.id      
-                FOR XML PATH('')      
-            ), 1, 1, ''),       
-        ']')      
-        ELSE '[]'      
-    END AS PromotionList,  -- 促銷活動名稱清單（JSON字串）     
-     
-    ISNULL(      
-    (      
-        -- 取第一個促銷活動類別名稱作為Badge顯示(促銷類別在後台沒有在用了，全部都在同一個類別下)     
-        SELECT TOP 1 gkind.Name      
-        FROM       
-            p_agiokind gkind      
-            JOIN P_AgioItems aitem       
-                ON gkind.EnterpriseID = @enterpriseId     
-                AND gkind.id = aitem.AgioKind      
-          JOIN P_AgioItems_Shop ashop       
-                ON aitem.EnterpriseID = @enterpriseId      
-                AND aitem.gid = ashop.MGID      
-            JOIN P_Agio_FoodKind_Mould amould       
-                ON amould.EnterpriseID = @enterpriseId      
-                AND amould.MouldCode = aitem.gid      
-            JOIN P_Agio_FoodMould afmould       
-                ON afmould.EnterpriseID = @enterpriseId      
-                AND afmould.MouldCode = amould.MouldCode      
-        WHERE       
-            gkind.EnterpriseID = @enterpriseId      
-            AND ashop.shopid = @shopId      
-            AND aitem.DiscountKind IN ('ASinglePercent', 'AsingleCost')      
-            AND afmould.id = FM.id      
-    ),''      
-    ) AS PromotionBadge,              -- 優惠Badge     
-    CAST(CASE WHEN FM.stop = 1 THEN 1 ELSE 0 END AS Bit) AS IsSoldOut  -- 是否停售 (1=true, 0或null=false)     
-FROM P_FoodMould_Shop FMS     
-    -- 關聯菜單主檔     
-    JOIN P_FoodMould_M FMM ON FMM.EnterPriseID = @enterpriseId      
-        AND FMM.MouldCode = FMS.MouldCode      
-        AND FMM.[Status] = 9     
-        AND FMM.MouldType = CASE @orderType     
-            WHEN 'takeout' THEN 2     
-            WHEN 'delivery' THEN 5      
-            WHEN 'scaneDesk' THEN 6     
-        END     
-    -- 關聯菜單食品主檔（YSFlag=1為套餐，0為單品）     
-    JOIN P_FoodMould FM ON FM.EnterPriseID = @enterpriseId AND FM.MouldCode = FMS.MouldCode AND (FM.stop = 0 OR FM.stop IS NULL)  
-    -- 關聯食品小分類     
-    JOIN P_FoodKind FK ON FK.EnterpriseID = @enterpriseId AND FK.ID = FM.Kind     
-    -- 關聯食品資料     
-    JOIN P_Food F ON F.EnterpriseID = @enterpriseId AND F.Kind = FM.Kind AND F.ID = FM.ID     
-    -- 多語系：食品小分類     
-    LEFT JOIN P_Data_Language_D LANGKIND ON LANGKIND.EnterpriseID = @enterpriseid AND LANGKIND.SourceID = FM.Kind AND LANGKIND.TableName = 'FoodKind'     
-    -- 多語系：食品名稱     
-    LEFT JOIN P_Data_Language_D LANGFOOD ON LANGFOOD.EnterpriseID = @enterpriseid AND LANGFOOD.SourceID = FM.ID AND LANGFOOD.TableName = 'Food'      
-WHERE FMS.EnterPriseID = @enterpriseId     
-    AND FMS.ShopID = @shopId     
-    AND (@foodId IS NULL OR FM.ID = @foodId) 
-ORDER BY FoodCategoryId, Sort;      
-    END TRY      
-    BEGIN CATCH      
-        -- 錯誤處理區，將錯誤訊息回傳給呼叫端     
-        DECLARE @ErrorMessage NVARCHAR(4000);      
-        DECLARE @ErrorSeverity INT;      
-        DECLARE @ErrorState INT;      
+                        AND PFM.Hide = 0
+                    LEFT JOIN P_Data_Language_D LANGPFM ON LANGPFM.EnterpriseID = @enterpriseId  
+                        AND LANGPFM.SourceID = PFM.ID  
+                        AND LANGPFM.TableName = 'Food' 
+                    WHERE PFEM.EnterpriseID = @enterpriseId  
+                        AND PFEM.MouldCode = FMS.MouldCode  
+                        AND PFEM.EntFood = FM.ID  -- 找出包含當前商品的套餐 
+                    FOR XML PATH('') 
+                ), 1, 1, ''), 
+            ']') 
+        ELSE '[]' 
+    END AS UpgradeList,      
+    FM.Sn AS Sort,               -- 排序      
       
-        SELECT       
-            @ErrorMessage = ERROR_MESSAGE(),      
-            @ErrorSeverity = ERROR_SEVERITY(),      
-            @ErrorState = ERROR_STATE();      
+    -- 促銷活動清單（若有多個促銷，組成JSON陣列字串）      
+    CASE        
+        WHEN EXISTS (       
+            SELECT 1       
+            FROM p_agiokind gkind_inner       
+            JOIN P_AgioItems aitem        
+                ON gkind_inner.EnterpriseID = @enterpriseId       
+                AND gkind_inner.id = aitem.AgioKind       
+            JOIN P_AgioItems_Shop ashop        
+                ON aitem.EnterpriseID = @enterpriseId       
+                AND aitem.gid = ashop.MGID       
+            JOIN P_Agio_FoodKind_Mould amould        
+                ON amould.EnterpriseID = @enterpriseId       
+                AND amould.MouldCode = aitem.gid       
+            JOIN P_Agio_FoodMould afmould        
+                ON afmould.EnterpriseID = @enterpriseId       
+                AND afmould.MouldCode = amould.MouldCode       
+            WHERE        
+                gkind_inner.EnterpriseID = @enterpriseId       
+                AND ashop.shopid = @shopId       
+                AND aitem.DiscountKind IN ('ASinglePercent', 'AsingleCost')       
+                AND afmould.id = FM.ID      
+    )       
+        THEN CONCAT('[',        
+            STUFF((       
+                SELECT DISTINCT       
+                    ',"' + aitem.Name + '"'       
+                FROM        
+                    p_agiokind gkind_inner       
+                    JOIN P_AgioItems aitem        
+                        ON gkind_inner.EnterpriseID = aitem.EnterpriseID       
+                        AND gkind_inner.id = aitem.AgioKind       
+                    JOIN P_AgioItems_Shop ashop        
+                        ON aitem.EnterpriseID = ashop.EnterpriseID       
+                        AND aitem.gid = ashop.MGID       
+                    JOIN P_Agio_FoodKind_Mould amould        
+                        ON amould.EnterpriseID = ashop.EnterpriseID       
+                        AND amould.MouldCode = aitem.gid       
+                    JOIN P_Agio_FoodMould afmould        
+                        ON afmould.EnterpriseID = ashop.EnterpriseID       
+                        AND afmould.MouldCode = amould.MouldCode       
+                WHERE        
+                    gkind_inner.EnterpriseID = @enterpriseId       
+                    AND ashop.shopid = @shopId       
+                    AND aitem.DiscountKind IN ('ASinglePercent', 'AsingleCost')       
+                    AND afmould.id = FM.id       
+                FOR XML PATH('')       
+            ), 1, 1, ''),        
+        ']')       
+        ELSE '[]'       
+    END AS PromotionList,  -- 促銷活動名稱清單（JSON字串）      
       
-        -- 返回錯誤信息      
-        RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);      
-    END CATCH;      
+    ISNULL(       
+    (       
+        -- 取第一個促銷活動類別名稱作為Badge顯示(促銷類別在後台沒有在用了，全部都在同一個類別下)      
+        SELECT TOP 1 gkind.Name       
+        FROM        
+            p_agiokind gkind       
+            JOIN P_AgioItems aitem        
+                ON gkind.EnterpriseID = @enterpriseId      
+                AND gkind.id = aitem.AgioKind       
+          JOIN P_AgioItems_Shop ashop        
+                ON aitem.EnterpriseID = @enterpriseId       
+                AND aitem.gid = ashop.MGID       
+            JOIN P_Agio_FoodKind_Mould amould        
+                ON amould.EnterpriseID = @enterpriseId       
+                AND amould.MouldCode = aitem.gid       
+            JOIN P_Agio_FoodMould afmould        
+                ON afmould.EnterpriseID = @enterpriseId       
+                AND afmould.MouldCode = amould.MouldCode       
+        WHERE        
+            gkind.EnterpriseID = @enterpriseId       
+            AND ashop.shopid = @shopId       
+            AND aitem.DiscountKind IN ('ASinglePercent', 'AsingleCost')       
+            AND afmould.id = FM.id       
+    ),''       
+    ) AS PromotionBadge,              -- 優惠Badge      
+    CAST(CASE WHEN FM.stop = 1 THEN 1 ELSE 0 END AS Bit) AS IsSoldOut  -- 是否停售 (1=true, 0或null=false)      
+FROM P_FoodMould_Shop FMS      
+    -- 關聯菜單主檔      
+    JOIN P_FoodMould_M FMM ON FMM.EnterPriseID = @enterpriseId       
+        AND FMM.MouldCode = FMS.MouldCode       
+        AND FMM.[Status] = 9      
+        AND FMM.MouldType = CASE @orderType      
+            WHEN 'takeout' THEN 2      
+            WHEN 'delivery' THEN 5       
+            WHEN 'scaneDesk' THEN 6      
+        END      
+    -- 關聯菜單食品主檔（YSFlag=1為套餐，0為單品）      
+    JOIN P_FoodMould FM ON FM.EnterPriseID = @enterpriseId AND FM.MouldCode = FMS.MouldCode AND (FM.stop = 0 OR FM.stop IS NULL) AND FM.Hide = 0 
+    -- 關聯食品小分類      
+    JOIN P_FoodKind FK ON FK.EnterpriseID = @enterpriseId AND FK.ID = FM.Kind      
+    -- 關聯食品資料      
+    JOIN P_Food F ON F.EnterpriseID = @enterpriseId AND F.Kind = FM.Kind AND F.ID = FM.ID      
+    -- 多語系：食品小分類      
+    LEFT JOIN P_Data_Language_D LANGKIND ON LANGKIND.EnterpriseID = @enterpriseid AND LANGKIND.SourceID = FM.Kind AND LANGKIND.TableName = 'FoodKind'      
+    -- 多語系：食品名稱      
+    LEFT JOIN P_Data_Language_D LANGFOOD ON LANGFOOD.EnterpriseID = @enterpriseid AND LANGFOOD.SourceID = FM.ID AND LANGFOOD.TableName = 'Food'       
+WHERE FMS.EnterPriseID = @enterpriseId      
+    AND FMS.ShopID = @shopId  
+    AND (@foodId IS NULL OR FM.ID = @foodId)  
+ORDER BY FoodCategoryId, Sort;       
+    END TRY       
+    BEGIN CATCH       
+        -- 錯誤處理區，將錯誤訊息回傳給呼叫端      
+        DECLARE @ErrorMessage NVARCHAR(4000);       
+        DECLARE @ErrorSeverity INT;       
+        DECLARE @ErrorState INT;       
+       
+        SELECT        
+            @ErrorMessage = ERROR_MESSAGE(),       
+            @ErrorSeverity = ERROR_SEVERITY(),       
+            @ErrorState = ERROR_STATE();       
+       
+        -- 返回錯誤信息       
+        RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);       
+    END CATCH;       
 END 
--- sp_GetShopList 取得門店清單.sql
+++ sp_GetShopList 取得門店清單.sql
@@ -1,29 +1,39 @@
-CREATE PROCEDURE [dbo].[sp_GetShopList]  
+CREATE OR ALTER PROCEDURE [dbo].[sp_GetShopList]  
     @enterpriseId NVARCHAR(50), -- 企業號Id  
     @userLat FLOAT,            -- 目前位置經度  
     @userLng FLOAT,            -- 目前位置緯度  
     @keyword NVARCHAR(100) = NULL, -- 門市關鍵字查詢  
-    @topN INT = 10             -- 取N筆 (預設10筆)  
+    @topN INT = 1000,             -- 取N筆 (預設10筆)
+    @modeID NVARCHAR(50) = 'takeout' -- 預設為取門店外帶設定
 AS  
+
+-- 測試用
+-- DECLARE 
+--     @enterpriseId NVARCHAR(50) = '90367984', -- 企業號Id  
+--     @userLat FLOAT = 22.643323954493976,            -- 目前位置經度  
+--     @userLng FLOAT = 120.31496755391419,            -- 目前位置緯度  
+--     @keyword NVARCHAR(100) = NULL, -- 門市關鍵字查詢  
+--     @topN INT = 1000,             -- 取N筆 (預設10筆)  
+-- 	@modeID NVARCHAR(50) = 'scaneDesk' -- 預設為取門店外帶設定
+
 BEGIN  
-    SET NOCOUNT ON;  
-      
---    SELECT TOP (@topN)  
---				'A02' AS shopId, -- 門市Id  
---        '惠饗製研所' AS ShopName,           -- 門市名稱  
---        '台北市信義區永吉路302號2樓之1' AS ShopAddress,        -- 門市地址  
---        1.55 AS Distance,           -- 距離 (KM)  
---        CAST(1 AS Bit) AS AcceptAppOrder        -- 是否接受APP訂單  
+    SET NOCOUNT ON;    
   
-	SELECT DISTINCT TOP (@topN)  
-		store.OrgCode AS ShopId,	--門市編號  
+	SELECT TOP (@topN)  
+		store.OrgCode AS ShopId,   -- 門市編號  
 	    store.OrgName AS ShopName, -- 門市名稱  
 	    store.Addr AS ShopAddress, -- 門市地址  
-	    store.Tel AS ShopTel,      -- 門市電話 
-	    CASE   
-	        WHEN IsRej.DefaultValue = 'true' THEN 0  
-	        ELSE 1  
-	    END AS AcceptAppOrder, -- 是否接受訂單  
+	    store.Tel AS ShopTel,      -- 門市電話
+        ISNULL((select 
+            CASE 
+                WHEN S.[Value] = 'true' THEN 'false'
+                WHEN S.[Value] = 'false' THEN 'true'
+            END
+            from S_AppSetting_Shop S
+            JOIN S_Appsetting_M M ON M.ModeID = @modeID
+            JOIN S_AppSetting_D D ON S.AppSetting_D_GID = D.GID AND D.AppSetting_M_GID = M.GID AND D.Name = 'IsRejectOrder'
+            WHERE EnterpriseID = @enterpriseId
+            AND ShopID = store.OrgCode), 'true') AS  AcceptAppOrder,     -- 是否接單  
 	    CASE   
 	        WHEN store.google_lat IS NOT NULL AND store.google_lng IS NOT NULL AND store.google_lat <> '' AND store.google_lng <> '' THEN  
 	            ROUND(6371 * ACOS(COS(RADIANS(@userLat)) * COS(RADIANS(store.google_lat)) * COS(RADIANS(store.google_lng) - RADIANS(@userLng)) + SIN(RADIANS(@userLat)) * SIN(RADIANS(store.google_lat))), 2)   
@@ -31,12 +41,9 @@ BEGIN
 	            9999 -- 顯示 9999  
 	    END AS DistanceKM -- 計算距離 (公里)  
 	FROM S_Organ store  
-	LEFT JOIN S_AppSetting_Shop b ON b.enterpriseid = store.EnterPriseID AND b.shopID = store.OrgCode  
-	LEFT JOIN S_AppSetting_D D ON b.AppSetting_D_GID = D.GID  
-	LEFT JOIN S_AppSetting_D IsRej ON IsRej.gid = D.GID AND IsRej.Name = 'IsRejectOrdr'  
 	WHERE store.EnterPriseID = @enterpriseid  
-	  AND store.OrgType = '1' -- OrgType = 1  
-	  AND store.OrgCode <> '9999' -- OrgCode 不等於 9999  
+	  AND store.OrgType = '1' -- 取門市  
+	  AND store.OrgCode <> '9999' -- OrgCode 不等於 9999(測試店)  
 	ORDER BY DistanceKM; -- 按距離排序  
   
 END 
