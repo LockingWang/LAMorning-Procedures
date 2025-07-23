8d7251baefd65a3b2cc6b67f539f565e4043f4ca
CongRen <boom0525300@gmail.com>
2025-07-23 12:33:23 +0800

    更新

--- sp_GetFoodOptionsBatch 取得商品選項.sql
+++ sp_GetFoodOptionsBatch 取得商品選項.sql
@@ -1,77 +1,77 @@
-CREATE OR ALTER PROCEDURE [dbo].[sp_GetFoodOptionsBatch]    
-    @enterpriseId NVARCHAR(50),    
-    @shopId NVARCHAR(50),    
-    @foodIds NVARCHAR(MAX),    
-    @orderType NVARCHAR(50),    
-    @langId NVARCHAR(50)    
-AS    
-   
-   
--- DECLARE   
---     @enterpriseId NVARCHAR(50) = 'XFenjoy',    
---     @shopId NVARCHAR(50) = 'A02',  
---     @foodIds NVARCHAR(MAX) = 'EN00132723',  
---     @orderType NVARCHAR(50) = 'takeout', 
---     @langId NVARCHAR(50) = 'TW'    
-   
-   
-BEGIN    
-    SET NOCOUNT ON;    
-        
-    -- 將逗號分隔的 foodIds 轉換為表格    
-    DECLARE @FoodIdsTable TABLE (FoodId NVARCHAR(50))    
-    INSERT INTO @FoodIdsTable    
-    SELECT value FROM STRING_SPLIT(@foodIds, ',')    
+CREATE   PROCEDURE [dbo].[sp_GetFoodOptionsBatch]     
+    @enterpriseId NVARCHAR(50),     
+    @shopId NVARCHAR(50),     
+    @foodIds NVARCHAR(MAX),     
+    @orderType NVARCHAR(50),     
+    @langId NVARCHAR(50)     
+AS     
     
-    SELECT    
-        F.FoodId,  
-        FAK.ID AS ItemId,    
-        ISNULL(JSON_VALUE(LANGKIND.Content, '$.' + @langId + '.Name'), FAK.Name) AS ItemName,    
-        (    
-            SELECT    
-                CAST(FA2.ID AS NVARCHAR(50)) AS FoodId,    
-                '' AS ImagePath,    
-                ISNULL(JSON_VALUE(LANGADD.Content, '$.' + @langId + '.Name'), FA2.Name) AS FoodName,    
-                '' AS Description,    
-                FA2.Price,    
-                FA2.SN AS Sort,    
-                ISNULL(PFMJ.Stop,0) AS IsSoldOut, 
-                FA2.Lock 
-            FROM P_FoodAdd_Mould FA2
-            JOIN P_Food PF2 ON PF2.EnterPriseID = @enterpriseid AND PF2.ID = F.FoodId
-            LEFT JOIN P_Data_Language_D LANGADD    
-                ON LANGADD.EnterpriseID = @enterpriseid    
-                AND LANGADD.SourceID = FA2.ID    
-                AND LANGADD.TableName = 'FoodAdd'  
-            LEFT JOIN P_FoodTasteAddMouldJoin PFMJ on PFMJ.EnterpriseID = @enterpriseId and PFMJ.TasteAddName = FA2.Name and PFMJ.ShopID = @shopId  
-            WHERE FA2.AddKindID = FAK.ID    
-            AND FA2.EnterpriseID = @enterpriseId    
-            AND (FA2.Owner = F.FoodId OR FA2.Owner = PF2.Kind)   
-            AND FA2.MouldCode = FMM.MouldCode  
-            ORDER BY FA2.SN    
-            FOR JSON PATH    
-        ) AS Items,    
-        FAK.needed AS MinSelectCount,  
-        FAK.MaxCount AS MaxSelectCount  
-    FROM @FoodIdsTable F  
-    -- 透過 P_FoodMould_Shop、P_FoodMould_M 找出目標菜單的 MouldCode  
-    JOIN P_FoodMould_Shop FMS ON FMS.EnterpriseID = @enterpriseId AND FMS.ShopID = @shopId  
-    JOIN P_FoodMould_M FMM ON FMM.EnterPriseID = @enterpriseId       
-        AND FMM.MouldCode = FMS.MouldCode       
-        AND FMM.[Status] = 9        
-        AND FMM.MouldType = CASE @orderType        
-            WHEN 'takeout' THEN 2        
-            WHEN 'delivery' THEN 5         
-            WHEN 'scaneDesk' THEN 6   
-        END   
-    JOIN P_FoodAdd_Mould FAM ON FAM.EnterpriseID = @enterpriseId AND FAM.MouldCode = FMM.MouldCode  
-    JOIN P_FoodAddKind FAK ON FAK.ID = FAM.AddKindID  
-    JOIN P_Food PF ON PF.EnterPriseID = @enterpriseid AND PF.ID = F.FoodId
-    -- 依據語系找出對應的顯示文字  
-    LEFT JOIN P_Data_Language_D LANGKIND    
-        ON LANGKIND.EnterpriseID = @enterpriseid    
-        AND LANGKIND.SourceID = FAK.ID    
-        AND LANGKIND.TableName = 'FoodAddKind'
-    WHERE (FAM.Owner = F.FoodId OR FAM.Owner = PF.Kind)
-    GROUP BY F.FoodId, FAK.ID, FAK.Name, FAK.MaxCount, FAK.needed, LANGKIND.Content, FMM.MouldCode  
+    
+-- DECLARE    
+--     @enterpriseId NVARCHAR(50) = 'XFenjoy',     
+--     @shopId NVARCHAR(50) = 'A02',   
+--     @foodIds NVARCHAR(MAX) = 'EN00132723',   
+--     @orderType NVARCHAR(50) = 'takeout',  
+--     @langId NVARCHAR(50) = 'TW'     
+    
+    
+BEGIN     
+    SET NOCOUNT ON;     
+         
+    -- 將逗號分隔的 foodIds 轉換為表格     
+    DECLARE @FoodIdsTable TABLE (FoodId NVARCHAR(50))     
+    INSERT INTO @FoodIdsTable     
+    SELECT value FROM STRING_SPLIT(@foodIds, ',')     
+     
+    SELECT     
+        F.FoodId,   
+        FAK.ID AS ItemId,     
+        ISNULL(JSON_VALUE(LANGKIND.Content, '$.' + @langId + '.Name'), FAK.Name) AS ItemName,     
+        (     
+            SELECT     
+                CAST(FA2.ID AS NVARCHAR(50)) AS FoodId,     
+                '' AS ImagePath,     
+                ISNULL(JSON_VALUE(LANGADD.Content, '$.' + @langId + '.Name'), FA2.Name) AS FoodName,     
+                '' AS Description,     
+                FA2.Price,     
+                FA2.SN AS Sort,     
+                ISNULL(PFMJ.Stop,0) AS IsSoldOut,  
+                FA2.Lock  
+            FROM P_FoodAdd_Mould FA2 
+            JOIN P_Food PF2 ON PF2.EnterPriseID = @enterpriseid AND PF2.ID = F.FoodId 
+            LEFT JOIN P_Data_Language_D LANGADD     
+                ON LANGADD.EnterpriseID = @enterpriseid     
+                AND LANGADD.SourceID = FA2.ID     
+                AND LANGADD.TableName = 'FoodAdd'   
+            LEFT JOIN P_FoodTasteAddMouldJoin PFMJ on PFMJ.EnterpriseID = @enterpriseId and PFMJ.TasteAddName = FA2.Name and PFMJ.ShopID = @shopId   
+            WHERE FA2.AddKindID = FAK.ID     
+            AND FA2.EnterpriseID = @enterpriseId     
+            AND (FA2.Owner = F.FoodId OR FA2.Owner = PF2.Kind)    
+            AND FA2.MouldCode = FMM.MouldCode   
+            ORDER BY FA2.SN     
+            FOR JSON PATH     
+        ) AS Items,     
+        FAK.needed AS MinSelectCount,   
+        FAK.MaxCount AS MaxSelectCount   
+    FROM @FoodIdsTable F   
+    -- 透過 P_FoodMould_Shop、P_FoodMould_M 找出目標菜單的 MouldCode   
+    JOIN P_FoodMould_Shop FMS ON FMS.EnterpriseID = @enterpriseId AND FMS.ShopID = @shopId   
+    JOIN P_FoodMould_M FMM ON FMM.EnterPriseID = @enterpriseId        
+        AND FMM.MouldCode = FMS.MouldCode        
+        AND FMM.[Status] = 9         
+        AND FMM.MouldType = CASE @orderType         
+            WHEN 'takeout' THEN 2         
+            WHEN 'delivery' THEN 5          
+            WHEN 'scaneDesk' THEN 6    
+        END    
+    JOIN P_FoodAdd_Mould FAM ON FAM.EnterpriseID = @enterpriseId AND FAM.MouldCode = FMM.MouldCode   
+    JOIN P_FoodAddKind FAK ON FAK.ID = FAM.AddKindID   
+    JOIN P_Food PF ON PF.EnterPriseID = @enterpriseid AND PF.ID = F.FoodId 
+    -- 依據語系找出對應的顯示文字   
+    LEFT JOIN P_Data_Language_D LANGKIND     
+        ON LANGKIND.EnterpriseID = @enterpriseid     
+        AND LANGKIND.SourceID = FAK.ID     
+        AND LANGKIND.TableName = 'FoodAddKind' 
+    WHERE (FAM.Owner = F.FoodId OR FAM.Owner = PF.Kind) 
+    GROUP BY F.FoodId, FAK.ID, FAK.Name, FAK.MaxCount, FAK.needed, LANGKIND.Content, FMM.MouldCode   
 END
