8f421861b194b688ebeccb73a36a0d7126d64ad3
CongRen <boom0525300@gmail.com>
2025-07-24 14:30:06 +0800

    更新

--- sp_GetOrderDetail 取得訂單資訊.sql
+++ sp_GetOrderDetail 取得訂單資訊.sql
@@ -1,60 +1,64 @@
-CREATE OR ALTER PROCEDURE [dbo].[sp_GetOrderDetail]  
-	@EnterPriseID NVARCHAR(50),  
-	@ShopID NVARCHAR(50),  
-    @memberNo NVARCHAR(50) = NULL,  -- 會員No   
-    @langId NVARCHAR(10) = NULL,  -- 語系Id   
-    @orderId NVARCHAR(50)   -- 訂單Id   
-AS   
-  
-BEGIN   
-	SET NOCOUNT ON;  
-  
-	-- 參數驗證  
-	IF @EnterPriseID IS NULL OR @ShopID IS NULL OR @orderId IS NULL  
-	BEGIN  
-		RAISERROR(N'缺少必要參數：EnterpriseID、ShopID 或 orderId 不能為空', 16, 1)  
-		RETURN  
-	END  
-  
-	SELECT  
+CREATE OR ALTER PROCEDURE [dbo].[sp_GetOrderDetail]    
+	@EnterPriseID NVARCHAR(50),    
+	@ShopID NVARCHAR(50),    
+    @memberNo NVARCHAR(50) = NULL,  -- 會員No     
+    @langId NVARCHAR(10) = NULL,  -- 語系Id     
+    @orderId NVARCHAR(50)   -- 訂單Id     
+AS     
+ 
+-- DECLARE @EnterPriseID NVARCHAR(50) = 'xurf',    
+-- 	@ShopID NVARCHAR(50) = 'A002',    
+--     @memberNo NVARCHAR(50) = '0903008556',  -- 會員No     
+--     @langId NVARCHAR(10) = 'TW',  -- 語系Id     
+--     @orderId NVARCHAR(50) = '7c4d846e-b4da-4dcb-8a20-f5bf8a556cde'   -- 訂單Id   
+    
+BEGIN     
+	SET NOCOUNT ON;    
+    
+	-- 參數驗證    
+	IF @EnterPriseID IS NULL OR @ShopID IS NULL OR @orderId IS NULL    
+	BEGIN    
+		RAISERROR(N'缺少必要參數：EnterpriseID、ShopID 或 orderId 不能為空', 16, 1)    
+		RETURN    
+	END    
+    
+	SELECT    
+		(    
+			SELECT     
+				o.*     
+			FROM P_OrdersTemp_Web o    
+			WHERE     
+				o.EnterPriseID = @EnterPriseID AND         
+				o.ID = @orderId    
+			FOR JSON PATH, WITHOUT_ARRAY_WRAPPER, INCLUDE_NULL_VALUES   
+		) AS [order],    
+		(    
+			SELECT     
+				i.*,   
+				(   
+					SELECT TOP 1 Dir   
+					FROM S_UploadFile uf    
+					WHERE uf.itemid = i.FoodID AND uf.enterpriseid = @EnterPriseID    
+				) AS imagePath   
+			FROM P_ItemsTemp_Web i    
+			INNER JOIN P_OrdersTemp_Web o ON     
+				i.EnterPriseID = o.EnterPriseID AND         
+				i.OrderID = o.ID    
+			WHERE     
+				o.EnterPriseID = @EnterPriseID AND         
+				o.ID = @orderId    
+			FOR JSON PATH, INCLUDE_NULL_VALUES   
+		) AS items,  
 		(  
 			SELECT   
-				o.*   
-			FROM P_OrdersTemp_Web o  
-			WHERE   
-				o.EnterPriseID = @EnterPriseID AND   
-				o.ShopID = @ShopID AND   
-				o.OrderNo2 = @orderId  
-			FOR JSON PATH, WITHOUT_ARRAY_WRAPPER, INCLUDE_NULL_VALUES 
-		) AS [order],  
-		(  
-			SELECT   
-				i.*, 
-				( 
-					SELECT TOP 1 Dir 
-					FROM S_UploadFile uf  
-					WHERE uf.itemid = i.FoodID AND uf.enterpriseid = @EnterPriseID  
-				) AS imagePath 
-			FROM P_ItemsTemp_Web i  
-			INNER JOIN P_OrdersTemp_Web o ON   
-				i.EnterPriseID = o.EnterPriseID AND   
-				i.ShopID = o.ShopID AND   
-				i.OrderID = o.ID  
-			WHERE   
-				o.EnterPriseID = @EnterPriseID AND   
-				o.ShopID = @ShopID AND   
-				o.OrderNo2 = @orderId  
-			FOR JSON PATH, INCLUDE_NULL_VALUES 
-		) AS items,
-		(
-			SELECT 
-				o.EnterPriseID,
-				o.OrgCode AS ShopID,
-				o.OrgName AS ShopName,
-				o.Tel AS ShopTel,
-				o.Addr AS ShopAddress
-			FROM S_Organ o
-			WHERE o.EnterPriseID = @EnterPriseID AND o.OrgCode = @ShopID
-			FOR JSON PATH, INCLUDE_NULL_VALUES
-		) AS shopInfo
+				o.EnterPriseID,  
+				o.OrgCode AS ShopID,  
+				o.OrgName AS ShopName,  
+				o.Tel AS ShopTel,  
+				o.Addr AS ShopAddress  
+			FROM S_Organ o 
+            JOIN P_OrdersTemp_Web orders ON orders.ID = @orderId 
+			WHERE o.EnterPriseID = @EnterPriseID AND o.OrgCode = orders.ShopID 
+			FOR JSON PATH, INCLUDE_NULL_VALUES  
+		) AS shopInfo  
 END 
--- sp_UpdateOrderPaymentStatus.sql
+++ sp_UpdateOrderPaymentStatus.sql
@@ -1,105 +1,69 @@
-CREATE OR ALTER PROCEDURE [dbo].[sp_UpdateOrderPaymentStatus] 
-    @EnterpriseId NVARCHAR(50), 
-    @ShopId NVARCHAR(50), 
-    @OrderId NVARCHAR(100), 
-    @PlatformOrderId NVARCHAR(100), 
-    @Status NVARCHAR(100), 
-    @Action NVARCHAR(100), 
-    @Amount DECIMAL(18, 2), 
-    @TransactionDate NVARCHAR(100), 
-    @errorCode INT OUTPUT, 
-    @errorMessage NVARCHAR(MAX) OUTPUT 
-AS 
- 
-BEGIN 
-    SET NOCOUNT ON; 
- 
-    -- 宣告變數來儲存轉換後的 Action 值
-    DECLARE @ConvertedAction NVARCHAR(100);
-    
-    -- 轉換 Action 值
-    SET @ConvertedAction = CASE @Action
-        WHEN 'SinopacWeb' THEN 'Sinopac'
-        WHEN 'LinePayWeb' THEN 'LINEpay_web'
-        WHEN 'JkoPayWeb' THEN 'JkoPay_web'
-        ELSE @Action
-    END;
- 
-    BEGIN TRY 
-        -- 參數驗證 
-        IF @EnterpriseId IS NULL OR @ShopId IS NULL OR @OrderId IS NULL OR @PlatformOrderId IS NULL 
-        BEGIN 
-            SET @errorCode = 1; 
-            SET @errorMessage = N'缺少必要參數：EnterpriseId、ShopId、OrderId 或 PlatformOrderId 不能為空'; 
-            RETURN; 
-        END 
- 
-        -- 檢查 Status 是否為 success 
-        IF @Status = 'success' 
-        BEGIN 
-            BEGIN TRANSACTION; 
- 
-            -- 更新 P_OrdersTemp_Web 表中的付款狀態 
-            UPDATE P_OrdersTemp_Web 
-            SET PayStatus = 1 
-            WHERE ID = @OrderId; 
- 
-            -- 將訂單資料寫入 P_ChecksTemp_Web 暫存表 
-            INSERT INTO P_ChecksTemp_Web ( 
-                GID, 
-                EnterpriseID, 
-                SHOPID, 
-                BatchID, 
-                OrderID, 
-                CheckID, 
-                CheckName, 
-                Total, 
-                Remark, 
-                LastModify, 
-                DeskID, 
-                ItemsData, 
-                PCommand, 
-                EDC, 
-                CheckNO 
-            ) 
-            VALUES ( 
-                NEWID(),                -- GID 隨機生成 
-                @EnterpriseId,          -- EnterpriseID 
-                @ShopId,                -- SHOPID 
-                @PlatformOrderId,       -- BatchID 
-                @OrderId,               -- OrderID 
-                @ConvertedAction,       -- CheckID (使用轉換後的值)
-                @ConvertedAction,       -- CheckName (使用轉換後的值)
-                @Amount,                -- Total 
-                @ConvertedAction,       -- Remark (使用轉換後的值)
-                GETDATE(),              -- LastModify 
-                NULL,                   -- DeskID 
-                NULL,                   -- ItemsData 
-                NULL,                   -- PCommand 
-                NULL,                   -- EDC 
-                NULL                    -- CheckNO 
-            ); 
- 
-            SET @errorCode = 0; 
-            SET @errorMessage = N'更新成功'; 
- 
-            COMMIT TRANSACTION; 
-        END 
-        ELSE 
-        BEGIN 
-            SET @errorCode = 0; 
-            SET @errorMessage = N'狀態不是 success，僅查詢訂單資訊'; 
-        END 
- 
-        -- 取得訂單資訊（無論 Status 為何都會執行） 
-        SELECT * FROM P_OrdersTemp_Web WHERE ID = @OrderId; 
- 
-    END TRY 
-    BEGIN CATCH 
-        IF @@TRANCOUNT > 0 
-            ROLLBACK TRANSACTION; 
- 
-        SET @errorCode = 1; 
-        SET @errorMessage = N'更新訂單狀態時發生錯誤：' + ERROR_MESSAGE(); 
-    END CATCH 
+CREATE   PROCEDURE [dbo].[sp_UpdateOrderPaymentStatus]  
+    @EnterpriseId NVARCHAR(50),  
+    @ShopId NVARCHAR(50),  
+    @OrderId NVARCHAR(100),  
+    @PlatformOrderId NVARCHAR(100),  
+    @Status NVARCHAR(100),  
+    @Action NVARCHAR(100),  
+    @Amount DECIMAL(18, 2),  
+    @TransactionDate NVARCHAR(100),  
+    @errorCode INT OUTPUT,  
+    @errorMessage NVARCHAR(MAX) OUTPUT  
+AS  
+  
+BEGIN  
+    SET NOCOUNT ON;  
+  
+    -- 宣告變數來儲存轉換後的 Action 值 
+    DECLARE @ConvertedAction NVARCHAR(100); 
+     
+    -- 轉換 Action 值 
+    SET @ConvertedAction = CASE @Action 
+        WHEN 'SinopacWeb' THEN 'Sinopac' 
+        WHEN 'LinePayWeb' THEN 'LINEpay_web' 
+        WHEN 'JkoPayWeb' THEN 'JkoPay_web' 
+        ELSE @Action 
+    END; 
+  
+    BEGIN TRY  
+        -- 參數驗證  
+        IF @EnterpriseId IS NULL OR @ShopId IS NULL OR @OrderId IS NULL OR @PlatformOrderId IS NULL  
+        BEGIN  
+            SET @errorCode = 1;  
+            SET @errorMessage = N'缺少必要參數：EnterpriseId、ShopId、OrderId 或 PlatformOrderId 不能為空';  
+            RETURN;  
+        END  
+  
+        -- 檢查 Status 是否為 success  
+        IF @Status = 'success'  
+        BEGIN  
+            BEGIN TRANSACTION;  
+  
+            -- 更新 P_OrdersTemp_Web 表中的付款狀態  
+            UPDATE P_OrdersTemp_Web  
+            SET PayStatus = 1  
+            WHERE ID = @OrderId;  
+  
+            SET @errorCode = 0;  
+            SET @errorMessage = N'更新成功';  
+  
+            COMMIT TRANSACTION;  
+        END  
+        ELSE  
+        BEGIN  
+            SET @errorCode = 0;  
+            SET @errorMessage = N'狀態不是 success，僅查詢訂單資訊';  
+        END  
+  
+        -- 取得訂單資訊（無論 Status 為何都會執行）  
+        SELECT * FROM P_OrdersTemp_Web WHERE ID = @OrderId;  
+  
+    END TRY  
+    BEGIN CATCH  
+        IF @@TRANCOUNT > 0  
+            ROLLBACK TRANSACTION;  
+  
+        SET @errorCode = 1;  
+        SET @errorMessage = N'更新訂單狀態時發生錯誤：' + ERROR_MESSAGE();  
+    END CATCH  
 END
