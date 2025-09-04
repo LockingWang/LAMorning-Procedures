cbf45476dd8f17d058c8e34a412c1e9bcd255635
CongRen <boom0525300@gmail.com>
2025-09-04 16:06:48 +0800

    更新 sp_GetComboItemsBatch、sp_GetFoodList 和 sp_GetFoodOptionsBatch 存儲過程，新增 mouldCodes 參數以支持菜單代碼清單，調整查詢邏輯以使用此參數，並移除不必要的時段菜單邏輯。

--- sp_GetComboItemsBatch 批量查詢附餐資訊.sql
+++ sp_GetComboItemsBatch 批量查詢附餐資訊.sql
@@ -2,44 +2,27 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-ALTER   PROCEDURE [dbo].[sp_GetComboItemsBatch]    
+ALTER PROCEDURE [dbo].[sp_GetComboItemsBatch]    
     @enterpriseId NVARCHAR(50),    
     @shopId NVARCHAR(50),    
     @foodIds NVARCHAR(MAX),    
-    @orderType NVARCHAR(50),    
-    @langId NVARCHAR(50)    
+    @langId NVARCHAR(50),
+    @mouldCodes NVARCHAR(MAX)      -- 菜單代碼清單（逗號分隔）
 AS    
    
 -- DECLARE @enterpriseId NVARCHAR(50) = 'XFlamorning',    
---     @shopId NVARCHAR(50) = '08Z01',    
---     @foodIds NVARCHAR(MAX) = 'A5648910',    
---     @orderType NVARCHAR(50) = 'scaneDesk',    
---     @langId NVARCHAR(50) = 'TW';   
+--     @shopId NVARCHAR(50) = '03F03',    
+--     @foodIds NVARCHAR(MAX) = 'A5648912',    
+--     @langId NVARCHAR(50) = 'TW',
+--     @mouldCodes NVARCHAR(MAX) = 'OnlineTogo_A1140801';   
    
 BEGIN    
     SET NOCOUNT ON;    
         
-    -- 宣告時段菜單變數
-    DECLARE @useTimeBasedMenu BIT = 0;
-    DECLARE @currentWeekDay INT;
-    DECLARE @currentTime TIME;
-    
-    -- 如果訂單類型是 scaneDesk，檢查是否需要使用時段菜單
-    IF @orderType = 'scaneDesk'
-    BEGIN
-        -- 取得當前星期和時間
-        SET @currentWeekDay = DATEPART(WEEKDAY, GETDATE());
-        SET @currentTime = CAST(GETDATE() AS TIME);
-        
-        -- 檢查是否使用時段菜單
-        SELECT @useTimeBasedMenu = CASE 
-            WHEN ParameterValue = 'true' THEN 1 
-            ELSE 0 
-        END
-        FROM S_Parameter_Enterprise 
-        WHERE EnterPriseID = @enterpriseId 
-            AND ParameterGID = '79dabbec-a224-4f34-a13e-04540c9d548e';
-    END
+    -- 將逗號分隔的 mouldCodes 轉換為表格    
+    DECLARE @MouldCodesTable TABLE (MouldCode NVARCHAR(50))    
+    INSERT INTO @MouldCodesTable    
+    SELECT value FROM STRING_SPLIT(@mouldCodes, ',')    
         
     -- 將逗號分隔的 foodIds 轉換為表格    
     DECLARE @FoodIdsTable TABLE (FoodId NVARCHAR(50))    
@@ -55,7 +38,7 @@ BEGIN
             SELECT DISTINCT    
                 ENT.EntFood AS FoodId,   
                 ISNULL(JSON_VALUE(LANGFOOD.Content, '$.' + @langId + '.Name'), PF.Name) AS FoodName,   
-                ISNULL(SUF.Dir, '') AS ImagePath,    
+                ISNULL(SUF.Dir, '') AS ImagePath, 
                 PF.Introduce AS Description,   
                 ENT.Price AS Price,   
                 ENT.EntNo AS Sort,   
@@ -67,22 +50,26 @@ BEGIN
             JOIN P_FoodMould PF   
                 ON ENT.EnterpriseID = PF.EnterpriseID   
                 AND ENT.EntFood = PF.ID   
-                AND PF.MouldCode = COALESCE(fm.MouldCode, fmt.MouldCode)   
+                AND PF.MouldCode = ENT.MouldCode   
                 AND PF.Kind = PEK.KindID  
                 AND (PF.Stop = 0 OR PF.Stop IS NULL)  
-            LEFT JOIN S_UploadFile SUF   
-                ON ENT.EnterpriseID = SUF.EnterpriseID   
-                AND SUF.ItemID = ENT.EntFood    
-                AND SUF.vType = 'food2'    
+            OUTER APPLY (
+                SELECT TOP 1 SUF.Dir
+                FROM S_UploadFile SUF
+                WHERE SUF.EnterpriseID = ENT.EnterpriseID
+                AND SUF.ItemID = ENT.EntFood
+                AND SUF.vType = 'food2'
+                ORDER BY SUF.LastModify DESC
+            ) SUF
             LEFT JOIN P_Data_Language_D LANGFOOD    
                 ON LANGFOOD.EnterpriseID = @enterpriseid    
                 AND LANGFOOD.SourceID = ENT.EntFood   
                 AND LANGFOOD.TableName = 'Food'  
             -- POS 商品售完   
-            LEFT JOIN P_FoodMouldJoin PFMJ on PFMJ.EnterpriseID = @enterpriseId and PFMJ.MouldCode = COALESCE(fm.MouldCode, fmt.MouldCode) and PFMJ.FoodID = ENT.EntFood and PFMJ.ShopID = @shopId  
+            LEFT JOIN P_FoodMouldJoin PFMJ on PFMJ.EnterpriseID = @enterpriseId and PFMJ.MouldCode = ENT.MouldCode and PFMJ.FoodID = ENT.EntFood and PFMJ.ShopID = @shopId  
             WHERE ENT.EnterpriseID = @enterpriseId    
                 AND ENT.MainFood = F.FoodId   
-                AND ENT.MouldCode = COALESCE(fm.MouldCode, fmt.MouldCode)   
+                AND ENT.MouldCode IN (SELECT MouldCode FROM @MouldCodesTable)   
                 AND ENT.EntFood IS NOT NULL    
             ORDER BY ENT.EntNo    
             FOR JSON PATH    
@@ -92,40 +79,8 @@ BEGIN
         PEK.EntKindNo AS Sort, 
         PEK.[Group] AS [Group] 
     FROM @FoodIdsTable F   
-    -- 先透過 P_FoodMould_M 以及 P_FoodMould_Shop 確定要取的菜單 MouldCode   
-    JOIN P_FoodMould_Shop fshop ON fshop.EnterpriseID = @enterpriseId AND fshop.ShopID = @shopId
-    -- 關聯菜單主檔 - 根據是否使用時段菜單選擇不同的表
-    LEFT JOIN P_FoodMould_M fm ON @useTimeBasedMenu = 0
-        AND fm.EnterpriseID = @enterpriseId   
-        AND fm.[Status] = 9   
-        AND fm.MouldCode = fshop.MouldCode
-        AND fm.MouldType = CASE @orderType          
-            WHEN 'takeout' THEN 2          
-            WHEN 'delivery' THEN 5           
-            WHEN 'scaneDesk' THEN 6          
-        END    
-    -- 如果使用時段菜單，則關聯 P_FoodMould_M_Time 表進行時間篩選
-    LEFT JOIN P_FoodMould_M_Time fmt ON @useTimeBasedMenu = 1 
-        AND fmt.EnterpriseID = @enterpriseId 
-        AND fmt.MouldCode = fshop.MouldCode
-        AND (
-            -- 檢查星期是否符合
-            (@currentWeekDay = 2 AND fmt.Week1 = 1) OR
-            (@currentWeekDay = 3 AND fmt.Week2 = 1) OR
-            (@currentWeekDay = 4 AND fmt.Week3 = 1) OR
-            (@currentWeekDay = 5 AND fmt.Week4 = 1) OR
-            (@currentWeekDay = 6 AND fmt.Week5 = 1) OR
-            (@currentWeekDay = 7 AND fmt.Week6 = 1) OR
-            (@currentWeekDay = 1 AND fmt.Week7 = 1)
-        )
-        AND (
-            -- 檢查時間是否符合任一時間區段
-            (@currentTime BETWEEN fmt.BeginTime1 AND fmt.EndTime1) OR
-            (@currentTime BETWEEN fmt.BeginTime2 AND fmt.EndTime2) OR
-            (@currentTime BETWEEN fmt.BeginTime3 AND fmt.EndTime3)
-        )   
     -- 取得*小類*的資訊與數量設定   
-    JOIN P_FoodEntKind_Mould PEK ON PEK.EnterpriseID = @enterpriseId AND PEK.MouldCode = COALESCE(fm.MouldCode, fmt.MouldCode) AND PEK.FoodID = F.FoodId   
+    JOIN P_FoodEntKind_Mould PEK ON PEK.EnterpriseID = @enterpriseId AND PEK.MouldCode IN (SELECT MouldCode FROM @MouldCodesTable) AND PEK.FoodID = F.FoodId   
     LEFT JOIN P_Data_Language_D LANGKIND   
         ON LANGKIND.EnterpriseID = @enterpriseid   
         AND LANGKIND.SourceID = PEK.KindID   
--- sp_GetFoodOptionsBatch 取得商品選項.sql
+++ sp_GetFoodOptionsBatch 取得商品選項.sql
@@ -2,46 +2,29 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-ALTER   PROCEDURE [dbo].[sp_GetFoodOptionsBatch]      
+ALTER PROCEDURE [dbo].[sp_GetFoodOptionsBatch]      
     @enterpriseId NVARCHAR(50),      
     @shopId NVARCHAR(50),      
     @foodIds NVARCHAR(MAX),      
-    @orderType NVARCHAR(50),      
-    @langId NVARCHAR(50)      
+    @langId NVARCHAR(50),
+    @mouldCodes NVARCHAR(MAX)      -- 菜單代碼清單（逗號分隔）
 AS        
   
 -- DECLARE      
---     @enterpriseId NVARCHAR(50) = 'XFenjoy',       
---     @shopId NVARCHAR(50) = 'A01',     
---     @foodIds NVARCHAR(MAX) = 'EN00132732',     
---     @orderType NVARCHAR(50) = 'takeout',    
---     @langId NVARCHAR(50) = 'TW'       
+--     @enterpriseId NVARCHAR(50) = 'xurf',     
+--     @shopId NVARCHAR(50) = 'A001',     
+--     @foodIds NVARCHAR(MAX) = 'la0134036',     
+--     @langId NVARCHAR(50) = 'TW',
+--     @mouldCodes NVARCHAR(MAX) = 'OnlineTogo_06000';       
      
      
 BEGIN      
     SET NOCOUNT ON;      
         
-    -- 宣告時段菜單變數
-    DECLARE @useTimeBasedMenu BIT = 0;
-    DECLARE @currentWeekDay INT;
-    DECLARE @currentTime TIME;
-    
-    -- 如果訂單類型是 scaneDesk，檢查是否需要使用時段菜單
-    IF @orderType = 'scaneDesk'
-    BEGIN
-        -- 取得當前星期和時間
-        SET @currentWeekDay = DATEPART(WEEKDAY, GETDATE());
-        SET @currentTime = CAST(GETDATE() AS TIME);
-        
-        -- 檢查是否使用時段菜單
-        SELECT @useTimeBasedMenu = CASE 
-            WHEN ParameterValue = 'true' THEN 1 
-            ELSE 0 
-        END
-        FROM S_Parameter_Enterprise 
-        WHERE EnterPriseID = @enterpriseId 
-            AND ParameterGID = '79dabbec-a224-4f34-a13e-04540c9d548e';
-    END
+    -- 將逗號分隔的 mouldCodes 轉換為表格    
+    DECLARE @MouldCodesTable TABLE (MouldCode NVARCHAR(50))    
+    INSERT INTO @MouldCodesTable    
+    SELECT value FROM STRING_SPLIT(@mouldCodes, ',')    
           
     -- 將逗號分隔的 foodIds 轉換為表格      
     DECLARE @FoodIdsTable TABLE (FoodId NVARCHAR(50), FoodKind NVARCHAR(50))      
@@ -52,40 +35,13 @@ BEGIN
     UPDATE F 
     SET F.FoodKind = PFM.Kind 
     FROM @FoodIdsTable F 
-    JOIN P_FoodMould_Shop PFMS ON PFMS.EnterpriseID = @enterpriseId AND PFMS.ShopID = @shopId
-    -- 關聯菜單主檔 - 根據是否使用時段菜單選擇不同的表
-    LEFT JOIN P_FoodMould_M PFMM ON @useTimeBasedMenu = 0
-        AND PFMM.EnterpriseID = @enterpriseId 
-        AND PFMM.MouldCode = PFMS.MouldCode
-        AND PFMM.[Status] = 9
-        AND PFMM.MouldType = CASE @orderType 
-            WHEN 'takeout' THEN 2 
-            WHEN 'delivery' THEN 5 
-            WHEN 'scaneDesk' THEN 6 
-        END 
-    -- 如果使用時段菜單，則關聯 P_FoodMould_M_Time 表進行時間篩選
-    LEFT JOIN P_FoodMould_M_Time PFMT ON @useTimeBasedMenu = 1 
-        AND PFMT.EnterpriseID = @enterpriseId 
-        AND PFMT.MouldCode = PFMS.MouldCode
-        AND (
-            -- 檢查星期是否符合
-            (@currentWeekDay = 2 AND PFMT.Week1 = 1) OR
-            (@currentWeekDay = 3 AND PFMT.Week2 = 1) OR
-            (@currentWeekDay = 4 AND PFMT.Week3 = 1) OR
-            (@currentWeekDay = 5 AND PFMT.Week4 = 1) OR
-            (@currentWeekDay = 6 AND PFMT.Week5 = 1) OR
-            (@currentWeekDay = 7 AND PFMT.Week6 = 1) OR
-            (@currentWeekDay = 1 AND PFMT.Week7 = 1)
-        )
-        AND (
-            -- 檢查時間是否符合任一時間區段
-            (@currentTime BETWEEN PFMT.BeginTime1 AND PFMT.EndTime1) OR
-            (@currentTime BETWEEN PFMT.BeginTime2 AND PFMT.EndTime2) OR
-            (@currentTime BETWEEN PFMT.BeginTime3 AND PFMT.EndTime3)
-        )
-    JOIN P_FoodMould PFM ON PFM.EnterpriseID = @enterpriseId AND PFM.MouldCode = PFMS.MouldCode
-    JOIN P_Food PF ON PF.enterpriseID = @enterpriseId AND PF.Kind = PFM.Kind AND PF.ID = F.FoodId AND PF.NoKindAdd = '0'
-    WHERE PFM.ID = F.FoodId 
+    JOIN P_FoodMould PFM ON PFM.EnterpriseID = @enterpriseId 
+        AND PFM.MouldCode IN (SELECT MouldCode FROM @MouldCodesTable)
+        AND PFM.ID = F.FoodId
+    JOIN P_Food PF ON PF.enterpriseID = @enterpriseId 
+        AND PF.Kind = PFM.Kind 
+        AND PF.ID = F.FoodId 
+        AND PF.NoKindAdd = '0' -- 不列入類別註記 = 1，有列入 = 0
   
     SELECT      
         F.FoodId,    
@@ -102,62 +58,32 @@ BEGIN
                 ISNULL(PFMJ.Stop,0) AS IsSoldOut,   
                 FA2.Lock   
             FROM P_FoodAdd_Mould FA2  
+            LEFT JOIN P_FoodAdd PFA ON PFA.EnterpriseID = @enterpriseId AND PFA.ID = FA2.ID
             LEFT JOIN P_Data_Language_D LANGADD      
                 ON LANGADD.EnterpriseID = @enterpriseid      
-                AND LANGADD.SourceID = FA2.ID      
+                AND LANGADD.SourceID = PFA.MGID      
                 AND LANGADD.TableName = 'FoodAdd'    
-            LEFT JOIN P_FoodTasteAddMouldJoin PFMJ on PFMJ.EnterpriseID = @enterpriseId and PFMJ.TasteAddName = FA2.Name and PFMJ.ShopID = @shopId    
+            LEFT JOIN P_FoodTasteAddMouldJoin PFMJ on PFMJ.EnterpriseID = @enterpriseId and PFMJ.TasteAddName = FA2.Name and PFMJ.ShopID = @shopId
             WHERE FA2.AddKindID = FAK.ID      
             AND FA2.EnterpriseID = @enterpriseId      
             AND (FA2.Owner = F.FoodId OR FA2.[Owner] = F.FoodKind) 
-            AND FA2.MouldCode = COALESCE(FMM.MouldCode, FMT.MouldCode)
+            AND FA2.MouldCode IN (SELECT MouldCode FROM @MouldCodesTable)
             ORDER BY FA2.SN      
             FOR JSON PATH      
         ) AS Items,      
         FAK.needed AS MinSelectCount,    
         FAK.MaxCount AS MaxSelectCount    
     FROM @FoodIdsTable F    
-    -- 透過 P_FoodMould_Shop、P_FoodMould_M 找出目標菜單的 MouldCode    
-    JOIN P_FoodMould_Shop FMS ON FMS.EnterpriseID = @enterpriseId AND FMS.ShopID = @shopId    
-    -- 關聯菜單主檔 - 根據是否使用時段菜單選擇不同的表
-    LEFT JOIN P_FoodMould_M FMM ON @useTimeBasedMenu = 0
-        AND FMM.EnterPriseID = @enterpriseId         
-        AND FMM.MouldCode = FMS.MouldCode         
-        AND FMM.[Status] = 9          
-        AND FMM.MouldType = CASE @orderType          
-            WHEN 'takeout' THEN 2          
-            WHEN 'delivery' THEN 5           
-            WHEN 'scaneDesk' THEN 6     
-        END     
-    -- 如果使用時段菜單，則關聯 P_FoodMould_M_Time 表進行時間篩選
-    LEFT JOIN P_FoodMould_M_Time FMT ON @useTimeBasedMenu = 1 
-        AND FMT.EnterpriseID = @enterpriseId 
-        AND FMT.MouldCode = FMS.MouldCode
-        AND (
-            -- 檢查星期是否符合
-            (@currentWeekDay = 2 AND FMT.Week1 = 1) OR
-            (@currentWeekDay = 3 AND FMT.Week2 = 1) OR
-            (@currentWeekDay = 4 AND FMT.Week3 = 1) OR
-            (@currentWeekDay = 5 AND FMT.Week4 = 1) OR
-            (@currentWeekDay = 6 AND FMT.Week5 = 1) OR
-            (@currentWeekDay = 7 AND FMT.Week6 = 1) OR
-            (@currentWeekDay = 1 AND FMT.Week7 = 1)
-        )
-        AND (
-            -- 檢查時間是否符合任一時間區段
-            (@currentTime BETWEEN FMT.BeginTime1 AND FMT.EndTime1) OR
-            (@currentTime BETWEEN FMT.BeginTime2 AND FMT.EndTime2) OR
-            (@currentTime BETWEEN FMT.BeginTime3 AND FMT.EndTime3)
-        )
-    JOIN P_FoodAdd_Mould FAM ON FAM.EnterpriseID = @enterpriseId AND FAM.MouldCode = COALESCE(FMM.MouldCode, FMT.MouldCode)    
+    JOIN P_FoodAdd_Mould FAM ON FAM.EnterpriseID = @enterpriseId 
+        AND FAM.MouldCode IN (SELECT MouldCode FROM @MouldCodesTable)    
     JOIN P_FoodAddKind FAK ON FAK.ID = FAM.AddKindID    
     -- 依據語系找出對應的顯示文字    
     LEFT JOIN P_Data_Language_D LANGKIND      
-        ON LANGKIND.EnterpriseID = @enterpriseid      
-        AND LANGKIND.SourceID = FAK.ID      
-        AND LANGKIND.TableName = 'FoodAddKind'  
+        ON LANGKIND.EnterpriseID = @enterpriseid
+        AND LANGKIND.SourceID = FAK.ID
+        AND LANGKIND.TableName = 'FoodAddKind'
     WHERE (FAM.Owner = F.FoodId OR FAM.[Owner] = F.FoodKind)
-    GROUP BY F.FoodId, F.FoodKind ,FAK.ID,FAK.SN, FAK.Name, FAK.MaxCount, FAK.needed, LANGKIND.Content, COALESCE(FMM.MouldCode, FMT.MouldCode)
+    GROUP BY F.FoodId, F.FoodKind ,FAK.ID,FAK.SN, FAK.Name, FAK.MaxCount, FAK.needed, LANGKIND.Content
     order by FAK.SN
 END
 GO
