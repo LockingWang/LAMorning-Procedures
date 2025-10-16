83e66acfd32e7e1d192ee63f8e050944cfd530e3
CongRen <boom0525300@gmail.com>
2025-10-16 11:39:22 +0800

    更新 sp_GetEnterpriseInfo 存儲過程，新增客服專線及 AppSMSDayTimes 參數的查詢邏輯，調整查詢以增強可讀性及維護性。

--- sp_GetEnterpriseInfo 取得企業資訊.sql
+++ sp_GetEnterpriseInfo 取得企業資訊.sql
@@ -14,6 +14,7 @@ BEGIN
     SELECT  
 	    -- 提取品牌Logo資訊 
     	so.OrgName AS EnterpriseName, 
+        so.Tel AS CustomerServicePhone,
 	    JSON_VALUE(config, '$.logoRectangle') AS LogoLarge, 
 	    JSON_VALUE(config, '$.logoSquare') AS LogoSmall, 
 	     
@@ -26,21 +27,25 @@ BEGIN
 	    JSON_VALUE(config, '$.colorButtonText') AS PrimaryButtonTextColor, 
 	    JSON_VALUE(config, '$.colorPrimary') AS SecondaryButtonColor, 
 	    JSON_VALUE(config, '$.colorSecondaryButtonText') AS SecondaryButtonTextColor, 
-	     
-	    -- 取得客服專線 (當 S_Organ 表的 EnterPriseID = OrgCode = 企業號Id) 
-	    /*(SELECT Tel FROM NCW_xurf.dbo.S_Organ  
-	     WHERE EnterPriseID = @enterpriseid AND OrgCode = @enterpriseid)*/so.Tel AS CustomerServicePhone,
+
+        -- 取得 AppSMSDayTimes 參數
+	    (SELECT ISNULL(pe.ParameterValue, ps.ParameterValue)
+	     FROM S_Parameter_System ps
+	     LEFT JOIN S_Parameter_Enterprise pe
+	         ON pe.ParameterGID = ps.GID
+	         AND pe.EnterPriseID = @enterpriseId
+	     WHERE ps.ParameterName = 'AppSMSDayTimes') AS AppSMSDayTimes,
 	     
 	    -- 取得AppSMSIntervalMin參數
 	    (SELECT ISNULL(pe.ParameterValue, ps.ParameterValue) AS AppSMSIntervalMin
-	     FROM NCW_xurf.dbo.S_Parameter_System ps
-	     LEFT JOIN NCW_xurf.dbo.S_Parameter_Enterprise pe
+	     FROM S_Parameter_System ps
+	     LEFT JOIN S_Parameter_Enterprise pe
 	         ON pe.ParameterGID = ps.GID
 	         AND pe.EnterPriseID = @enterpriseId
 	     WHERE ps.ParameterName = 'AppSMSIntervalMin') AS AppSMSIntervalMin 
-	FROM NCW_xurf.dbo.System_Enterprise se 
-	LEFT JOIN NCW_xurf.dbo.T_SystemSetting ts ON ts.EnterpriseID = @enterpriseId AND ts.OrgCode = @enterpriseId
-	LEFT JOIN NCW_xurf.dbo.S_Organ so on so.EnterPriseID=@enterpriseId and so.OrgType=5 and so.OrgCode=@enterpriseId -- 企業名稱改為抓S_Organ避免修改後顯示錯誤
+	FROM System_Enterprise se 
+	LEFT JOIN T_SystemSetting ts ON ts.EnterpriseID = @enterpriseId AND ts.OrgCode = @enterpriseId
+	LEFT JOIN S_Organ so on so.EnterPriseID=@enterpriseId and so.OrgType=5 and so.OrgCode=@enterpriseId -- 企業名稱改為抓S_Organ避免修改後顯示錯誤
 	WHERE se.EnterpriseID = @enterpriseid;
  
 END
